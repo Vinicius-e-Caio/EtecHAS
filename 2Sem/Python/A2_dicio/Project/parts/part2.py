17e7aeb40bed8bfe8bd73cdda3a8fb5757737ab6
Vinicius-e-Caio <viniciusjesusfilho05@gmail.com>
2024-08-24 19:39:47 -0300

    feat: function to edit students

--- 2Sem/Python/A2_dicio/Project/parts/part2.py
+++ 2Sem/Python/A2_dicio/Project/parts/part2.py
@@ -1,8 +1,27 @@
-def editarAluno(nomeAluno, newNota) -> None:
+import os
+os.system("cls")
 
+def editarAluno(nomeAluno:  str, newNota: str) -> None:
+    newNota = newNota.replace(",", ".")
+    newNota = float(newNota)
+    if newNota < 0 or newNota > 10:
+        print("Nota inválida.\n")
+        return None
+    found = False
+    for keys in notas.keys():
+        if keys.lower() == nomeAluno.lower():
+            found = True
+    if not found:
+        print(f"Aluno {nomeAluno.lower()} não encontrado!")
+        return 
+    try:
+        notas[nomeAluno] = newNota
+        print(f"Nota do aluno {nomeAluno.lower()} alterada com sucesso!")    
+    except:
+        print(f"Ocorreu um erro.")
 
 notas = {
-    'lara2': 1.0,
+    'lara': 1.0,
     'lara3': 1.0,
     'lara4': 1.0,
     'lara5': 1.0,
@@ -11,7 +30,10 @@ notas = {
     'lara8': 1.0,
     'lara9': 1.0
 }
-aluno = str(input("Digite o Nome do aluno para alterar o aluno: "))
+aluno = str(input("Digite o nome do aluno para alterar sua nota: "))
 if not aluno.isalpha():
     print("Nome inválido!!")
-aluno = float(input("Digite a nova nota: "))
+nota = input("Digite a nova nota: ")
+if not nota.isdigit():
+    print("Nota inválida!!")
+editarAluno(aluno, nota)
